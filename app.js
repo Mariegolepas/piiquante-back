84f1a372356cf6d1758c7a7ce265cf5501d7beee
Mariegolepas <97367355+Mariegolepas@users.noreply.github.com>
2022-12-31 02:42:13 +0100

    init app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
+const path = require('path');
+
+mongoose.connect('mongodb+srv://adminuser:736HzJemjj2NbV@cluster0.jmmxck8.mongodb.net/Piiquante?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+mongoose.set('strictQuery', false);
+
+const app = express();
+app.use(express.json()); //accès au corps de la requête
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+module.exports = app;
